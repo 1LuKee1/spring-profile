3263f7c8cebfc31cb4ff5676af8bc4c878316525
Łukasz Kręc <lukasz.krec@onet.pl>
2023-02-10 15:12:09 +0100

    Initial commit

--- src/main/java/com/example/springprofile/vehicle/VehicleService.java
+++ src/main/java/com/example/springprofile/vehicle/VehicleService.java
@@ -35,12 +35,12 @@ class VehicleService {
     @Transactional
     public void deleteVehicleById(Long id) {
         if (id == 0) {
-            throw new RuntimeException("You need to provide ID of student to be deleted. ID can not be 0.");
+            throw new RuntimeException("You need to provide ID of vehicle to be deleted. ID can not be 0.");
         }
-        Optional<Vehicle> checkIfStudentWithIdExist = repository.findById(id);
-        if (checkIfStudentWithIdExist.isEmpty()) {
+        Optional<Vehicle> checkIfVehicleWithIdExist = repository.findById(id);
+        if (checkIfVehicleWithIdExist.isEmpty()) {
             throw new RuntimeException(
-                    "Student can not be deleted because student with id: " + id + " doesn't exist."
+                    "Vehicle can not be deleted because vehicle with id: " + id + " doesn't exist."
             );
         }
         repository.deleteVehicleById(id);
